dbd8a0e22635664c37ee2816e249a452552b8bc9
Ruslan Akmeev <Akmeev-RR@mail.ca.sbrf.ru>
2018-05-30 15:20:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroTest01
+{
+
+	class Neuron
+	{
+		double[] A;
+		double[] W;
+		double Bias;
+
+		public Neuron()
+		{
+			Bias = 7;
+			A = new double[15];
+			W = new double[15];
+			for (int i = 0; i < 15; i++)
+			{
+				A[i] = 0;
+				W[i] = 0;
+			}
+		}
+
+		public bool Do(string st)
+		{
+			for (int i = 0; i < 15; i++) A[i] = Convert.ToDouble(st.Substring(i,1));
+			double result=0;
+			for (int i = 0; i < 15; i++)
+			{
+				result = result + A[i] * W[i];
+			}
+			//Console.WriteLine("result=" + result);
+			return (result >= Bias);
+		}
+
+		public void ChW(bool IncFlag)
+		{
+			//Console.WriteLine("ChW " + IncFlag.ToString());
+			double d = -1;
+			if (IncFlag) d = 1;
+			for (int i=1;i<15;i++)
+			{
+				if (A[i] == 1) W[i] = W[i] + d; // W[i] * 0.1 * d;
+				//Console.WriteLine("A[i]=" + A[i] + ", W[i]=" + W[i] + ", d=" + d);
+			}
+		}
+
+		public void Print()
+		{
+			Console.WriteLine("Start");
+			for (int i=0;i<15;i++)
+			{
+				Console.WriteLine("W(" + i + ") = " + W[i]);
+			}
+			Console.WriteLine("End");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] GoodData = {
+			"111101101101111", // 0
+			"001001001001001", // 1
+			"111001111100111", // 2
+			"111001111001111", // 3
+			"101101111001001", // 4
+			"111100111001111", // 5
+			"111100111101111", // 6
+			"111001001001001", // 7
+			"111101111101111", // 8
+			"111101111001111"  // 9
+			};
+
+			string[] WrongData = {
+			"111100111000111",
+			"111100010001111",
+			"111100011001111",
+			"110100111001111",
+			"110100111001011",
+			"111100101001111"
+			};
+
+			Neuron N = new Neuron();
+			N.Print();
+			Random random = new Random();
+			for (int i=0;i<50000;i++)
+			{
+				// learinig circle for good data
+				Console.Write("%d%%    \r"+i);
+				int j = random.Next(0, 9);
+				//Console.WriteLine("j=" + j);
+				bool r = N.Do(GoodData[j]);
+				if (!r && j==5) N.ChW(true); // Результат (-) Вход (+) Up.
+				if (r && j!=5) N.ChW(false); // Результат (+) Вход (-) Down.
+			}
+			N.Print();
+
+			// Checking ...
+			for (int i=0;i<10;i++)
+			{
+				bool r = N.Do(GoodData[i]);
+				Console.WriteLine("i="+i+" r="+r.ToString());
+			}
+
+			for (int i = 0; i < 6; i++)
+			{
+				bool r = N.Do(WrongData[i]);
+				Console.WriteLine("i=" + i + " r=" + r.ToString());
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ext_source.py
@@ -0,0 +1,71 @@
+﻿# Инициализация весов сети
+weights = []
+for i in range(15):
+weights.append(0)	
+# Порог функции активации
+32.	bias = 7
+33.	
+34.	# Является ли данное число 5
+35.	def proceed(number):
+36.	# Рассчитываем взвешенную сумму
+37.	net = 0
+38.	for i in range(15):
+39.	net += int(number[i])*weights[i]
+40.	
+41.	# Превышен ли порог? (Да - сеть думает, что это 5. Нет - сеть думает, что это другая цифра)
+42.	return net >= bias
+43.	
+44.	# Уменьшение значений весов, если сеть ошиблась и выдала 1
+45.	def decrease(number):
+46.	for i in range(15):
+47.	# Возбужденный ли вход
+48.	if int(number[i]) == 1:
+49.	# Уменьшаем связанный с ним вес на единицу
+50.	weights[i] -= 1
+51.	
+52.	# Увеличение значений весов, если сеть ошиблась и выдала 0
+53.	def increase(number):
+54.	for i in range(15):
+55.	# Возбужденный ли вход
+56.	if int(number[i]) == 1:
+57.	# Увеличиваем связанный с ним вес на единицу
+58.	weights[i] += 1
+59.	
+60.	# Тренировка сети
+61.	for i in range(10000):
+62.	# Генерируем случайное число от 0 до 9
+63.	option = random.randint(0, 9)
+64.	
+65.	# Если получилось НЕ число 5
+66.	if option != 5:
+67.	# Если сеть выдала True/Да/1, то наказываем ее
+68.	if proceed(nums[option]):
+69.	decrease(nums[option])
+70.	# Если получилось число 5
+71.	else:
+72.	# Если сеть выдала False/Нет/0, то показываем, что эта цифра - то, что нам нужно
+73.	if not proceed(num5):
+74.	increase(num5)
+75.	
+76.	# Вывод значений весов
+77.	print(weights)
+78.	
+79.	# Прогон по обучающей выборке
+80.	print("0 это 5? ", proceed(num0))
+81.	print("1 это 5? ", proceed(num1))
+82.	print("2 это 5? ", proceed(num2))
+83.	print("3 это 5? ", proceed(num3))
+84.	print("4 это 5? ", proceed(num4))
+85.	print("6 это 5? ", proceed(num6))
+86.	print("7 это 5? ", proceed(num7))
+87.	print("8 это 5? ", proceed(num8))
+88.	print("9 это 5? ", proceed(num9), '\n')
+89.	
+90.	# Прогон по тестовой выборке
+91.	print("Узнал 5? ", proceed(num5))
+92.	print("Узнал 5 - 1? ", proceed(num51))
+93.	print("Узнал 5 - 2? ", proceed(num52))
+94.	print("Узнал 5 - 3? ", proceed(num53))
+95.	print("Узнал 5 - 4? ", proceed(num54))
+96.	print("Узнал 5 - 5? ", proceed(num55))
+97.	print("Узнал 5 - 6? ", proceed(num56))
