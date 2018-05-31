1e8bfa6206fdbab028acbfb98a31c8f9c43c59d7
Ruslan Akmeev <Akmeev-RR@mail.ca.sbrf.ru>
2018-05-31 08:16:23 +0300

    ОБновление с некоторыми комментариями.

--- Program.cs
+++ Program.cs
@@ -6,34 +6,33 @@ using System.Threading.Tasks;
 
 namespace NeuroTest01
 {
-
 	class Neuron
 	{
+		Guid id;
+		ulong LearnCycleCount;
+		ulong UseCycleCount;
 		double[] A;
 		double[] W;
 		double Bias;
 
 		public Neuron()
 		{
+			id = Guid.NewGuid();
+			LearnCycleCount = 0;
+			UseCycleCount = 0;
 			Bias = 7;
 			A = new double[15];
 			W = new double[15];
-			for (int i = 0; i < 15; i++)
-			{
-				A[i] = 0;
-				W[i] = 0;
-			}
+			for (int i = 0; i < 15; i++) { A[i] = 0; W[i] = 0; }
 		}
 
 		public bool Do(string st)
 		{
 			for (int i = 0; i < 15; i++) A[i] = Convert.ToDouble(st.Substring(i,1));
 			double result=0;
-			for (int i = 0; i < 15; i++)
-			{
-				result = result + A[i] * W[i];
-			}
+			for (int i = 0; i < 15; i++) result = result + A[i] * W[i];
 			//Console.WriteLine("result=" + result);
+			UseCycleCount++;
 			return (result >= Bias);
 		}
 
@@ -44,27 +43,29 @@ namespace NeuroTest01
 			if (IncFlag) d = 1;
 			for (int i=1;i<15;i++)
 			{
-				if (A[i] == 1) W[i] = W[i] + d; // W[i] * 0.1 * d;
+				if (A[i] == 1) W[i] = W[i] + d; //W[i] / 2 * d;
 				//Console.WriteLine("A[i]=" + A[i] + ", W[i]=" + W[i] + ", d=" + d);
 			}
+			LearnCycleCount++;
 		}
 
 		public void Print()
 		{
-			Console.WriteLine("Start");
+			Console.WriteLine("Start Neuron ID = " + id);
 			for (int i=0;i<15;i++)
 			{
-				Console.WriteLine("W(" + i + ") = " + W[i]);
+				Console.Write("  W(" + i + ") = " + W[i]);
 			}
-			Console.WriteLine("End");
+			Console.WriteLine();
+			Console.WriteLine("Learn\\Use Cycles Count = " + LearnCycleCount + " \\ " + UseCycleCount);
+
+			Console.WriteLine("End Neuron ID = " + id);
 		}
 	}
 
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			string[] GoodData = {
+		static string[] GoodData = {
 			"111101101101111", // 0
 			"001001001001001", // 1
 			"111001111100111", // 2
@@ -75,44 +76,57 @@ namespace NeuroTest01
 			"111001001001001", // 7
 			"111101111101111", // 8
 			"111101111001111"  // 9
-			};
+		}; // Правильные цифры формата 3*5.
 
-			string[] WrongData = {
+		static string[] WrongData = {
 			"111100111000111",
 			"111100010001111",
 			"111100011001111",
 			"110100111001111",
 			"110100111001011",
 			"111100101001111"
-			};
+		}; // Несколько вариантов немного кривоватых цифр "5".
 
-			Neuron N = new Neuron();
-			N.Print();
+		static void TrainNeuronForDigit(Neuron N, int Cycles, int Value)
+		{
 			Random random = new Random();
-			for (int i=0;i<50000;i++)
+			for (int i = 0; i < Cycles; i++)
 			{
-				// learinig circle for good data
-				Console.Write("%d%%    \r"+i);
-				int j = random.Next(0, 9);
+				Console.Write("\r" + i); // /r это backspace - чтобы счетчик отрабатывал в одной строке.
+				int j = random.Next(0, 10);
 				//Console.WriteLine("j=" + j);
 				bool r = N.Do(GoodData[j]);
-				if (!r && j==5) N.ChW(true); // Результат (-) Вход (+) Up.
-				if (r && j!=5) N.ChW(false); // Результат (+) Вход (-) Down.
+				if (!r && j == Value) N.ChW(true); // Результат (-) Вход (+) Up.
+				if (r && j != Value) N.ChW(false); // Результат (+) Вход (-) Down.
 			}
-			N.Print();
+			Console.WriteLine();
+		}
 
-			// Checking ...
-			for (int i=0;i<10;i++)
+		static void CheckNeuronForDigit(Neuron N)
+		{
+			for (int i = 0; i < 10; i++)
 			{
 				bool r = N.Do(GoodData[i]);
-				Console.WriteLine("i="+i+" r="+r.ToString());
+				Console.WriteLine("i=" + i + " -> r=" + r.ToString());
 			}
 
 			for (int i = 0; i < 6; i++)
 			{
 				bool r = N.Do(WrongData[i]);
-				Console.WriteLine("i=" + i + " r=" + r.ToString());
+				Console.WriteLine("i=" + i + " -> r=" + r.ToString());
 			}
+		}
+
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++) {
+				Neuron N = new Neuron();
+				N.Print();
+				TrainNeuronForDigit(N, 100000, i);
+				N.Print();
+				CheckNeuronForDigit(N);
+			}
+
 			Console.ReadLine();
 		}
 	}
